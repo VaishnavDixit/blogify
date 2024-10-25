b172becfb996d9316a098f5a8c70a55835e99172
Vaishnav <vaishnavdixit98@gmail.com>
2024-10-26 01:11:30 +0530

    http ->https

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -32,8 +32,8 @@ export class AuthService {
         try {
             this.account.createOAuth2Session(
                 "google",
-                "http://blogify1234.netlify.app",
-                "http://blogify1234.netlify.app/error"
+                "https://blogify1234.netlify.app",
+                "https://blogify1234.netlify.app/error"
             );
         } catch (err) {
             console.log(err);
